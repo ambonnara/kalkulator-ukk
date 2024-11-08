9b7b6df9a732ba17d57a380c151b0e6a8d3dfa30
ambonnara <khoirulwafaaa@gmail.com>
2024-11-08 16:36:54 +0700

    Update Ambon.py

--- Ambon.py
+++ Ambon.py
@@ -1,79 +1,103 @@
 from tkinter import*
 window = Tk()
 
+#Fungsi Untuk Tombol
 def tombol_klik(item):
 	global ekspresi
 	ekspresi=ekspresi + str(item)
 	input_text.set(ekspresi)
 
+#Fungsi Tombol Hapus
 def tombol_hapus(): 
     global ekspresi
     ekspresi = "" 
-    input_teks.set("")
-    
-def tombol_samadengan():
+    input_text.set("")
+
+#Fungsi Tombol SamaDengan
+def tombol_samadengan():	
+	global ekspresi    
+	
+	try:
+		hasil = str(eval(ekspresi.replace('%', '/100')))
+		input_text.set(hasil)
+		ekspresi = ""
+	
+	except:
+		input_text.set("Error")
+		ekspresi = ""
+
+command=lambda: tombol_klik("%")
+
+#Fungsi Tombol Positive Negative
+def tombol_negasi():
     global ekspresi
-    hasil = str(eval(ekspresi)) 
-    input_teks.set(hasil)
-    ekspresi = ""
-    
+    if ekspresi:
+        try:
+            angka_terakhir = eval(ekspresi)
+            ekspresi = str(-angka_terakhir)
+            input_text.set(ekspresi)
+        except Exception as e:
+            input_text.set("Error")
+            ekspresi = ""
+            
 ekspresi=""
 input_text=StringVar()
  
-frame=Frame(window,bg="yellow",width=500,height=300)
+ #Frame Kalkulator
+frame=Frame(window,bg="black",width=500,height=300)
 frame.pack()
 
-frame2=Frame(window,bg="black",width=500,height=300)
+frame2=Frame(window,bg="gray",width=500,height=300)
 frame2.pack()
 
-label=Label(frame,text="Kalkulator Python",
+label=Label(frame,text="Kalkulator Wafa",width=50,anchor="w",
 font=("Arial",10))
 label.pack(padx=20,pady=20)
 
-label2=Label(frame,textvariable=input_text,
-font=("Arial",10))
+label2=Label(frame,textvariable=input_text,width=30,anchor="e",bg="gray",fg="white",font=("Arial",20))
 label2.pack(padx=20,pady=20)
 
 
+#Button Simbol
+Button(frame2,text="AC",command=lambda:tombol_hapus() ,fg="white",bg="black",font=("Arial",15)).grid(row=0,column=0,padx=10,pady=10)
 
-Button(frame2,text="C",command=lambda:tombol_hapus(),width=7,fg="white",bg="red").grid(row=0,column=0,columnspan=2)
+Button(frame2,text="%",command=lambda:tombol_klik("%"),bg="black",fg="white",font=("Arial",15)).grid(row=0,column=2,padx=10,pady=10)
 
-Button(frame2,text="/",command=lambda:tombol_klik("/"),bg="gray").grid(row=0,column=2,padx=10,pady=10)
+Button(frame2,text="± ",command=lambda:tombol_negasi(),bg="black",fg="white",font=("Arial",15)).grid(row=0,column=1,padx=10,pady=10)
 
-Button(frame2,text="*",command=lambda:tombol_klik("*"),bg="gray").grid(row=0,column=3,padx=10,pady=10)
+Button(frame2,text="/",command=lambda:tombol_klik("/"),bg="orange",fg="white",font=("Arial",15)).grid(row=0,column=3,padx=10,pady=10)
 
-Button(frame2,text="+",command=lambda:tombol_klik("+"),bg="gray").grid(row=1,column=3,padx=10,pady=10)
+Button(frame2,text="*",command=lambda:tombol_klik("*"),bg="orange",fg="white",font=("Arial",15)).grid(row=1,column=3,padx=10,pady=10)
 
-Button(frame2,text="-",command=lambda:tombol_klik("-"),bg="gray").grid(row=2,column=3,padx=10,pady=10)
+Button(frame2,text="-",command=lambda:tombol_klik("-"),bg="orange",fg="white",font=("Arial",15)).grid(row=2,column=3,padx=10,pady=10)
 
-Button(frame2,text="=",command=lambda:tombol_samadengan(),height=3,bg="gray").grid(row=3,column=3,rowspan=2)
+Button(frame2,text="=",command=lambda:tombol_samadengan(),bg="orange",fg="white",font=("Arial",15)).grid(row=4,column=3,padx=10,pady=10)
 
-Button(frame2,text=".",command=lambda:tombol_klik(".")).grid(row=4,column=2,padx=10,pady=10)
+Button(frame2,text="+",command=lambda:tombol_klik("+"),bg="orange",fg="white",font=("Arial",15)).grid(row=3,column=3,padx=10,pady=10)
 
-
-
-Button(frame2,text="0",command=lambda:tombol_klik("0"),width=7).grid(row=4,column=0,columnspan=2)
-
-
-Button(frame2,text="1",command=lambda:tombol_klik("1")).grid(row=3,column=0,padx=10,pady=10)
+Button(frame2,text=".",command=lambda:tombol_klik("."),bg="gray",fg="white",font=("Arial",15)).grid(row=4,column=2,padx=10,pady=10)
 
 
-Button(frame2,text="2",command=lambda:tombol_klik("2")).grid(row=3,column=1,padx=10,pady=10)
+#Button Angka
+Button(frame2,text="0",command=lambda:tombol_klik("0"),width=7,bg="gray",fg="white",font=("Arial",15)).grid(row=4,column=0,columnspan=2)
 
+Button(frame2,text="1",command=lambda:tombol_klik("1"),bg="gray",fg="white",font=("Arial",15)).grid(row=3,column=0,padx=10,pady=10)
 
-Button(frame2,text="3",command=lambda:tombol_klik("3")).grid(row=3,column=2,padx=10,pady=10)
+Button(frame2,text="2",command=lambda:tombol_klik("2"),bg="gray",fg="white",font=("Arial",15)).grid(row=3,column=1,padx=10,pady=10)
 
-Button(frame2,text="4",command=lambda:tombol_klik("4")).grid(row=2,column=0,padx=10,pady=10)
+Button(frame2,text="3",command=lambda:tombol_klik("3"),bg="gray",fg="white",font=("Arial",15)).grid(row=3,column=2,padx=10,pady=10)
 
-Button(frame2,text="5",command=lambda:tombol_klik("5")).grid(row=2,column=1,padx=10,pady=10)
+Button(frame2,text="4",command=lambda:tombol_klik("4"),bg="gray",fg="white",font=("Arial",15)).grid(row=2,column=0,padx=10,pady=10)
 
-Button(frame2,text="6",command=lambda:tombol_klik("6")).grid(row=2,column=2,padx=10,pady=10)
+Button(frame2,text="5",command=lambda:tombol_klik("5"),bg="gray",fg="white",font=("Arial",15)).grid(row=2,column=1,padx=10,pady=10)
 
-Button(frame2,text="7",command=lambda:tombol_klik("7")).grid(row=1,column=0,padx=10,pady=10)
+Button(frame2,text="6",command=lambda:tombol_klik("6"),bg="gray",fg="white",font=("Arial",15)).grid(row=2,column=2,padx=10,pady=10)
 
-Button(frame2,text="8",command=lambda:tombol_klik("8")).grid(row=1,column=1,padx=10,pady=10)
+Button(frame2,text="7",command=lambda:tombol_klik("7"),bg="gray",fg="white",font=("Arial",15)).grid(row=1,column=0,padx=10,pady=10)
 
-Button(frame2,text="9",command=lambda:tombol_klik("9")).grid(row=1,column=2,padx=10,pady=10)
+Button(frame2,text="8",command=lambda:tombol_klik("8"),bg="gray",fg="white",font=("Arial",15)).grid(row=1,column=1,padx=10,pady=10)
+
+Button(frame2,text="9",command=lambda:tombol_klik("9"),bg="gray",fg="white",font=("Arial",15)).grid(row=1,column=2,padx=10,pady=10)
 
 
 
